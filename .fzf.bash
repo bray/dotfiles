02a9c7e466bd512ca3cb223ba0376de256f2f9bf
Brian Ray <brian.ray1@gmail.com>
2021-04-15 09:53:13 -0400

    Set up FZF for bash (servers)

--- .fzf.bash
+++ .fzf.bash
@@ -1,14 +1,27 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == */home/ubuntu/.fzf/bin* ]]; then
+    export PATH="${PATH:+${PATH}:}/home/ubuntu/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/home/ubuntu/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.bash"
+source "/home/ubuntu/.fzf/shell/key-bindings.bash"
 
+
+# bray Customizations
+# -------------------
+export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
+
+export FZF_DEFAULT_OPTS='--height 40% --layout=reverse'
+
+#export FZF_CTRL_R_OPTS='--sort'
+#export FZF_CTRL_R_OPTS='--sort --exact'
+
+export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
