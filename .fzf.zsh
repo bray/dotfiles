d4eea2211e5c5d3b067f72a0503cdc188891525a
Brian Ray <brian.ray1@gmail.com>
2021-05-17 14:06:40 -0400

    Use fzf's Ctrl-T and Alt-C

--- .fzf.zsh
+++ .fzf.zsh
@@ -16,12 +16,9 @@ source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 # bray Customizations
 # -------------------
 export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
-
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse'
+
+export FZF_CTRL_T_OPTS="--preview '(bat --color=always --style=numbers --line-range=:200 {} || tree -C {}) 2> /dev/null'"
 
 #export FZF_CTRL_R_OPTS='--sort'
 #export FZF_CTRL_R_OPTS='--sort --exact'
-
-export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
