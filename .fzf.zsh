672449e2bc881913a4d5437114b6b1ab6bfd63cf
Brian Ray <brian.ray1@gmail.com>
2021-05-17 14:16:50 -0400

    Forgot this

--- .fzf.zsh
+++ .fzf.zsh
@@ -19,6 +19,7 @@ export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse'
 
 export FZF_CTRL_T_OPTS="--preview '(bat --color=always --style=numbers --line-range=:200 {} || tree -C {}) 2> /dev/null'"
+export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
 
 #export FZF_CTRL_R_OPTS='--sort'
 #export FZF_CTRL_R_OPTS='--sort --exact'
