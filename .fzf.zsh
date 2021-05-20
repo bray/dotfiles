d046e9698539066d63591d025dfc417c705236ff
Brian Ray <brian.ray1@gmail.com>
2021-05-20 14:35:31 -0400

    Add --hidden and --follow flags

--- .fzf.zsh
+++ .fzf.zsh
@@ -15,7 +15,7 @@ source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 
 # bray Customizations
 # -------------------
-export FZF_DEFAULT_COMMAND="fd --type f --exclude .git"
+export FZF_DEFAULT_COMMAND="fd --type f --hidden --follow --exclude .git"
 export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --multi --bind='ctrl-d:half-page-down,ctrl-u:half-page-up' --pointer='▶' --marker='✗'"
 
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
@@ -24,7 +24,7 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_PREVIEW_OPTS="(bat --color=always --style=numbers --line-range=:200 {} || tree -C {}) 2> /dev/null"
 export FZF_CTRL_T_OPTS="--preview '${FZF_CTRL_T_PREVIEW_OPTS}'"
 
-export FZF_ALT_C_COMMAND="fd --type d --exclude .git"
+export FZF_ALT_C_COMMAND="fd --type d --hidden --follow --exclude .git"
 export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
 
 #export FZF_CTRL_R_OPTS='--sort'
