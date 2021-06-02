0c166995e98fa0db5436236356c3f0709e8106e8
Brian Ray <brian.ray1@gmail.com>
2021-06-02 11:29:47 -0400

    Copy FZF config changes from zsh to bash

--- .fzf.bash
+++ .fzf.bash
@@ -8,6 +8,14 @@ fi
 # ---------------
 [[ $- == *i* ]] && source "${HOME}/.fzf/shell/completion.bash" 2> /dev/null
 
+_fzf_compgen_path() {
+    fd --hidden --follow . "$1"
+}
+_fzf_compgen_dir() {
+    fd --type d --hidden --follow . "$1"
+}
+
+
 # Key bindings
 # ------------
 source "${HOME}/.fzf/shell/key-bindings.bash"
@@ -15,10 +23,16 @@ source "${HOME}/.fzf/shell/key-bindings.bash"
 
 # bray Customizations
 # -------------------
-export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
-export FZF_DEFAULT_OPTS='--height 40% --layout=reverse'
+export FZF_DEFAULT_COMMAND="fd --type f --hidden --follow --exclude .git"
+export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --multi --bind='ctrl-d:half-page-down,ctrl-u:half-page-up' --pointer='▶' --marker='✗'"
 
-export FZF_CTRL_T_OPTS="--preview '(bat --color=always --style=numbers --line-range=:200 {} || tree -C {}) 2> /dev/null'"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+# Keep this split up so $FZF_CTRL_T_PREVIEW_OPTS can be used elsewhere, e.g. ~/.vimrc
+export FZF_CTRL_T_PREVIEW_OPTS="(bat --color=always --style=numbers --line-range=:200 {} || tree -C {}) 2> /dev/null"
+export FZF_CTRL_T_OPTS="--preview '${FZF_CTRL_T_PREVIEW_OPTS}'"
+
+export FZF_ALT_C_COMMAND="fd --type d --hidden --follow --exclude .git"
 export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
 
 #export FZF_CTRL_R_OPTS='--sort'
