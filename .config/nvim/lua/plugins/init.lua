acb3dbbb38b27b36d92ca72af1355754b5d7f30e
Brian Ray <brian.ray1@gmail.com>
2023-05-16 21:25:38 -0400

    Move Telescope config from init to config

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -44,14 +44,6 @@ require('lazy').setup({
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-      -- TODO: add whatever else is useful
-    end,
   },
   {
     'nvim-telescope/telescope-fzf-native.nvim',
