4e2ee19aa89ae016dbe1926ba92bc122b3031403
Brian Ray <brian.ray1@gmail.com>
2023-05-17 21:25:03 -0400

    Update a couple plugin comments

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -18,39 +18,39 @@ vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
   {
-    'drewtempelmeyer/palenight.vim',  -- Color theme
+    'drewtempelmeyer/palenight.vim',                 -- Color theme
     lazy = false,
     priority = 1000,
     config = function()
       vim.cmd.colorscheme 'palenight'
     end,
   },
-  'kana/vim-arpeggio',                         -- Better jk -> <Esc>
-  'vim-airline/vim-airline',                   -- Statusline config
-  'vim-airline/vim-airline-themes',            -- Statusline themes
-  'edkolev/tmuxline.vim',                      -- Needed only to update tmux statusline
-  'tpope/vim-fugitive',                        -- Various :Git commands
-  { 'lewis6991/gitsigns.nvim', opts = {} },    -- Git diff markers in the sign column and other integration
+  'kana/vim-arpeggio',                               -- Better jk -> <Esc>
+  'vim-airline/vim-airline',                         -- Statusline config
+  'vim-airline/vim-airline-themes',                  -- Statusline themes
+  'edkolev/tmuxline.vim',                            -- Needed only to update tmux statusline
+  'tpope/vim-fugitive',                              -- Various :Git commands
+  { 'lewis6991/gitsigns.nvim', opts = {} },          -- Git diff markers in the sign column and other integration
   {
-    'folke/which-key.nvim',                    -- Show popup of keybindings when typing
+    'folke/which-key.nvim',                          -- Show popup of keybindings when typing
     config = function()
       vim.o.timeout = true
       vim.o.timeoutlen = 300
       require('which-key').setup()
     end,
   },
-  { 'numToStr/Comment.nvim', opts = {} },      -- gc to comment/uncomment lines/blocks
-  'jeffkreeftmeijer/vim-numbertoggle',         -- Tiny plugin for better line numbers
+  { 'numToStr/Comment.nvim', opts = {} },            -- gc to comment/uncomment lines/blocks
+  'jeffkreeftmeijer/vim-numbertoggle',               -- Tiny plugin for better line numbers
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',  -- Find, Filter, Preview, Pick
     dependencies = { 'nvim-lua/plenary.nvim' },
   },
   {
-    'nvim-telescope/telescope-fzf-native.nvim',
+    'nvim-telescope/telescope-fzf-native.nvim',      -- Enable fzf in Telescope
     build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
   },
   {
-    'jremmen/vim-ripgrep',
+    'jremmen/vim-ripgrep',                           -- ripgrep in vim
     config = function()
       vim.g.rg_highlight = true
     end,
