77ddab1b79473b15f64cb2e8252c90457cf93561
Brian Ray <brian.ray1@gmail.com>
2023-05-19 16:34:53 -0400

    Add vim-rhubarb plugin

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -30,6 +30,7 @@ require('lazy').setup({
   'vim-airline/vim-airline-themes',                  -- Statusline themes
   'edkolev/tmuxline.vim',                            -- Needed only to update tmux statusline
   'tpope/vim-fugitive',                              -- Various :Git commands
+  'tpope/vim-rhubarb',                               -- Enables :GBrowse to open files/lines on Github
   { 'lewis6991/gitsigns.nvim', opts = {} },          -- Git diff markers in the sign column and other integration
   {
     'folke/which-key.nvim',                          -- Show popup of keybindings when typing
