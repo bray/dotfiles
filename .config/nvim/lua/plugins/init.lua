c672d1c6fe96de452b13e07431688ba944638358
Brian Ray <brian.ray1@gmail.com>
2023-06-29 14:00:15 -0400

    Add reminder of tmuxline usage

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -17,7 +17,7 @@ vim.opt.rtp:prepend(lazypath)
 
 
 require('lazy').setup({
-  'edkolev/tmuxline.vim',                            -- Needed only to update tmux statusline
+  'edkolev/tmuxline.vim',                            -- Needed only to update tmux statusline; run :Tmuxline airline, then :TmuxlineSnapshot
   'jeffkreeftmeijer/vim-numbertoggle',               -- Tiny plugin for better line numbers
   'kana/vim-arpeggio',                               -- Better jk -> <Esc>
   'smartpde/telescope-recent-files',                 -- MRU files via Telescope
