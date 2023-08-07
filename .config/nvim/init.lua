599ceaea70163821c3946c2b4fc440fc771c0be0
Brian Ray <brian.ray1@gmail.com>
2023-08-07 14:44:12 -0400

    Begin migrating init.vim (.vimrc) to lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,5 @@
+-- Source the old init.vim (.vimrc) file
+-- This will be removed once all of vimrc.vim is
+-- migrated to lua
+local vimrc = vim.fn.stdpath('config') .. '/vimrc.vim'
+vim.cmd.source(vimrc)
