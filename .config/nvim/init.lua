9b3c25a9bff657c4241faa3b676878ced2d17cb8
Brian Ray <brian.ray1@gmail.com>
2023-11-02 23:50:42 -0400

    All ported over!!

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,3 @@
--- Source the old init.vim (.vimrc) file
--- This will be removed once all of vimrc.vim is
--- migrated to lua
-local vimrc = vim.fn.stdpath('config') .. '/vimrc.vim'
-vim.cmd.source(vimrc)
-
 require 'options'
 require 'plugins'
 require 'commands'
