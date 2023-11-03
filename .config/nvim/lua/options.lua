620334426748e2ef652e0459a0bc9b2160f0d3f2
Brian Ray <brian.ray1@gmail.com>
2023-11-02 23:07:44 -0400

    Uniform spacing

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,29 +1,31 @@
 -- Change leader key
 vim.g.mapleader = ','
 
-vim.opt.breakindent = true                          -- Every wrapped line will continue visually indented
-vim.opt.complete:remove('t')                        -- Don't search tags when doing i_Ctrl-n and i_Ctrl-p
-vim.opt.cursorline = true                           -- Highlight the current line
-vim.opt.fillchars:append('eob: ')                   -- Remove the annoying ~ at the end of a buffer
-vim.opt.history = 2000                              -- Remember 2000 lines of history
-vim.opt.number = true                               -- Show line numbers
-vim.opt.shortmess:append('I')                       -- Don't show startup screen when starting vim
-vim.opt.showmode = false                            -- Don't show -- INSERT -- in bottom line (vim-airline shows this in status line)
-vim.opt.signcolumn = 'yes'                          -- Always show the sign column (e.g. for gitsigns)
-vim.opt.ttimeoutlen = 0                             -- Make escape go back to normal mode instantly
-vim.opt.foldenable = false                          -- Disable folding
-vim.opt.iskeyword:append('-')                       -- Treat hyphens in words as part of the word
-vim.opt.undofile = true                             -- Persist undo history
+vim.opt.breakindent = true          -- Every wrapped line will continue visually indented
+vim.opt.complete:remove('t')        -- Don't search tags when doing i_Ctrl-n and i_Ctrl-p
+vim.opt.cursorline = true           -- Highlight the current line
+vim.opt.fillchars:append('eob: ')   -- Remove the annoying ~ at the end of a buffer
+vim.opt.history = 2000              -- Remember 2000 lines of history
+vim.opt.number = true               -- Show line numbers
+vim.opt.shortmess:append('I')       -- Don't show startup screen when starting vim
+vim.opt.showmode = false            -- Don't show -- INSERT -- in bottom line (vim-airline shows this in status line)
+vim.opt.signcolumn = 'yes'          -- Always show the sign column (e.g. for gitsigns)
+vim.opt.ttimeoutlen = 0             -- Make escape go back to normal mode instantly
+vim.opt.colorcolumn = '81'          -- Show a column marker to show 81 characters is too many
+vim.opt.foldenable = false          -- Disable folding
+-- vim.opt.foldminlines = 99999	    --  needed?
+vim.opt.iskeyword:append('-')       -- Treat hyphens in words as part of the word
+vim.opt.undofile = true             -- Persist undo history
 
 
 --------------------------------------------------------------------------------
 -- Tabs & Spaces
 --------------------------------------------------------------------------------
 
-vim.opt.expandtab = true      -- Expand tab character to spaces
-vim.opt.softtabstop = 2       -- Indent by 2 spaces when pressing <TAB>
-vim.opt.shiftwidth = 2        -- Indent by 2 spaces when using >>, <<, == etc.
-vim.opt.autoindent = true     -- Keep indentation from previous line
+vim.opt.expandtab = true    -- Expand tab character to spaces
+vim.opt.softtabstop = 2     -- Indent by 2 spaces when pressing <TAB>
+vim.opt.shiftwidth = 2      -- Indent by 2 spaces when using >>, <<, == etc.
+vim.opt.autoindent = true   -- Keep indentation from previous line
 
 
 --------------------------------------------------------------------------------
@@ -40,8 +42,8 @@ vim.opt.hlsearch = true
 -- Splits
 --------------------------------------------------------------------------------
 
-vim.opt.splitright = true  -- Put a new vertical split window on the right
-vim.opt.splitbelow = true  -- Put a new horizontal split window below
+vim.opt.splitright = true   -- Put a new vertical split window on the right
+vim.opt.splitbelow = true   -- Put a new horizontal split window below
 
 
 vim.g.python3_host_prog = '~/.asdf/shims/python3'   -- Change python3 path to use asdf shim
