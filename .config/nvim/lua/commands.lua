42f324e17b19c0e378f6451ca2259c948e81e4b3
Brian Ray <brian.ray1@gmail.com>
2023-11-22 23:23:25 -0500

    Add CopyRelativePath command

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -1,6 +1,9 @@
 -- Copy full file path to clipboard
 vim.api.nvim_create_user_command('CopyFullPath', 'let @+ = expand("%:p")', {})
 
+-- Copy relative file path to clipboard
+vim.api.nvim_create_user_command('CopyRelativePath', 'let @+ = expand("%")', {})
+
 -- Restore the line and window positions when re-opening a buffer
 -- (instead of only the line position)
 vim.cmd([[
