370648c4ce6b54ac13c711c60b704d208283610f
Brian Ray <brian.ray1@gmail.com>
2023-12-28 16:00:42 -0500

    Use an array

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 
 get_staged_ruby_files() {
-  git diff --name-only --diff-filter=ACMR --cached | grep -E '.rb$' | tr '\n' ' '
+  git diff --name-only --diff-filter=ACMR --cached | grep -E '.rb$'
 }
 
 # Run rubocop on only git staged files
 # Can add an optional -a or -A flag
 rubocop_staged() {
-  staged_files=$(get_staged_ruby_files)
+  staged_files=($(get_staged_ruby_files))
 
-  if [[ ! -z "$staged_files" ]]; then
-    bundle exec rubocop $(echo $staged_files) $1
+  if [[ ${#staged_files[@]} -gt 0 ]]; then
+    bin/rubocop "${staged_files[@]}" $1
   else
     echo "No files are staged!"
   fi
