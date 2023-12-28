d0bb63f60de4e85356b52c53facc23f8393aa69c
Brian Ray <brian.ray1@gmail.com>
2023-12-28 14:51:28 -0500

    Extract `get_staged_ruby_files` function

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -1,9 +1,13 @@
 #!/usr/bin/env sh
 
+get_staged_ruby_files() {
+  git diff --name-only --diff-filter=ACMR --cached | grep -E '.rb$' | tr '\n' ' '
+}
+
 # Run rubocop on only git staged files
 # Can add an optional -a or -A flag
 rubocop_staged() {
-  staged_files=$(git diff --name-only --diff-filter=ACMR --cached | grep -E '.rb$' | tr '\n' ' ')
+  staged_files=$(get_staged_ruby_files)
 
   if [[ ! -z "$staged_files" ]]; then
     bundle exec rubocop $(echo $staged_files) $1
