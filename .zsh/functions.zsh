c35e4d4c3150e255305118c39d983438cbcbb30f
Brian Ray <brian.ray1@gmail.com>
2023-12-28 16:35:07 -0500

    DRY it up

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -4,33 +4,28 @@ get_staged_ruby_files() {
   git diff --name-only --diff-filter=ACMR --cached | grep -E '.rb$'
 }
 
-# Run rubocop on only git staged files
-# Can add an optional -a or -A flag
-rubocop_staged() {
+# Run a command on only git staged Ruby files
+run_cmd_on_staged_files() {
+  local cmd=$1
+  shift
   local staged_files=($(get_staged_ruby_files))
 
   if [[ ${#staged_files[@]} -gt 0 ]]; then
-    echo "Running rubocop on these files:\n"
+    echo "Running ${cmd} on these files:\n"
     printf '%s\n' "${staged_files[@]}"
     echo
 
-    bin/rubocop "${staged_files[@]}" "$@"
+    "$cmd" "${staged_files[@]}" "$@"
   else
     echo "No Ruby files are staged!"
   fi
 }
 
-# Run rspec on only git staged files
+# Can add an optional -a or -A flag
+rubocop_staged() {
+  run_cmd_on_staged_files bin/rubocop "$@"
+}
+
 rspec_staged() {
-  local staged_files=($(get_staged_ruby_files))
-
-  if [[ ${#staged_files[@]} -gt 0 ]]; then
-    echo "Running rspec on these files:\n"
-    printf '%s\n' "${staged_files[@]}"
-    echo
-
-    bin/rspec "${staged_files[@]}" "$@"
-  else
-    echo "No Ruby files are staged!"
-  fi
+  run_cmd_on_staged_files bin/rspec "$@"
 }
