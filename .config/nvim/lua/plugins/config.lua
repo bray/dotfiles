211723a30e75f6edb39436f8126e199c057e6d95
Brian Ray <brian.ray1@gmail.com>
2024-07-26 15:18:15 -0400

    Add 'mini.files' plugin to replace 'oil'

--- .config/nvim/lua/plugins/config.lua
+++ .config/nvim/lua/plugins/config.lua
@@ -221,6 +221,61 @@ require('mini.surround').setup({
 
 
 --------------------------------------------------------------------------------
+-- mini.files
+--------------------------------------------------------------------------------
+
+require('mini.files').setup({
+  -- Customization of explorer windows
+  windows = {
+    -- Whether to show preview of file/directory under cursor
+    -- preview = true,
+    -- Width of focused window
+    width_focus = 50,
+    -- Width of non-focused window
+    width_nofocus = 25,
+    -- Width of preview window
+    width_preview = 25,
+  },
+})
+
+local minifiles_toggle = function(...)
+  if not MiniFiles.close() then MiniFiles.open(vim.api.nvim_buf_get_name(0)) end
+end
+
+vim.keymap.set('n', '<leader>d', minifiles_toggle)
+
+
+-- TODO: tweak this to 1) create a new tab instead of a split, then 2) open the buffer in the new tab (and focus it)
+local map_split = function(buf_id, lhs, direction)
+  local rhs = function()
+    -- Make new window and set it as target
+    local new_target_window
+    vim.api.nvim_win_call(MiniFiles.get_target_window(), function()
+      vim.cmd(direction .. ' split')
+      new_target_window = vim.api.nvim_get_current_win()
+    end)
+
+    MiniFiles.set_target_window(new_target_window)
+  end
+
+  -- Adding `desc` will result into `show_help` entries
+  local desc = 'Split ' .. direction
+  vim.keymap.set('n', lhs, rhs, { buffer = buf_id, desc = desc })
+end
+
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'MiniFilesBufferCreate',
+  callback = function(args)
+    local buf_id = args.data.buf_id
+    -- Tweak keys to your liking
+    map_split(buf_id, '<C-t>', 'belowright horizontal')
+    -- map_split(buf_id, 'gv', 'belowright vertical')
+  end,
+})
+
+
+
+--------------------------------------------------------------------------------
 -- mini.pick
 --------------------------------------------------------------------------------
 
