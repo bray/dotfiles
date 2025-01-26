c6b4d7dd5da3f9c7c0c698a5980823e84a032215
Brian Ray <brian.ray1@gmail.com>
2025-01-25 23:20:37 -0500

    Lighter visual mode bg color

--- .config/nvim/lua/highlights.lua
+++ .config/nvim/lua/highlights.lua
@@ -1,12 +1,16 @@
--- Set the style for the current line
+-- Current line
 vim.api.nvim_set_hl(0, 'CursorLine', {
   bg = 'Grey15',
   bold = true
 })
 
--- Set the style for the current line in QuickFix mode
+-- QuickFix mode
 vim.api.nvim_set_hl(0, 'QuickFixLine', {
   bg = '#334659',
   ctermbg = 256
 })
 
+-- Lighter visual mode
+vim.api.nvim_set_hl(0, 'Visual', {
+  bg = '#4e576a'
+})
