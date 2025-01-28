9211a9a61287cd96f40426ee301e3ffd572491df
Brian Ray <brian.ray1@gmail.com>
2025-01-28 10:19:09 -0500

    Stop Homebrew from autoupgrading everything

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -1,1 +1,4 @@
 export RIPGREP_CONFIG_PATH="$XDG_CONFIG_HOME/ripgrep/config"
+
+# Stop Homebrew from autoupgrading everything when something in their dependency tree changes
+export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
