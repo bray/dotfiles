46c0fdbd712a1a0cdb36409b36bc82eca17aadab
Brian Ray <brian.ray1@gmail.com>
2025-04-02 22:59:01 -0400

    Some fix for $TMUX

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -2,3 +2,8 @@ export RIPGREP_CONFIG_PATH="$XDG_CONFIG_HOME/ripgrep/config"
 
 # Stop Homebrew from autoupgrading everything when something in their dependency tree changes
 export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
+
+# Don't know what changed, but this is now needed
+# Somehow, $TMUX is not getting set outside of tmux, which causes a
+# "no current client" error
+[ -z "$TMUX" ] || unset TMUX
