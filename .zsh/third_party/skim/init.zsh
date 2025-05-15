4573e13ebc8b0ff4de72b5fa0b4b329435b0788b
Brian Ray <brian.ray1@gmail.com>
2025-05-15 14:44:08 -0400

    Control load order

--- .zsh/third_party/skim/init.zsh
+++ .zsh/third_party/skim/init.zsh
@@ -1,11 +1,16 @@
+# Set default options
 preview='([[ -f {} ]] && (bat --style=numbers --color=always {} || cat {})) || ([[ -d {} ]] && (tree -L 2 -a -C {} | less || echo {} 2> /dev/null | head -20))'
 export SKIM_DEFAULT_OPTIONS="--color=16 --reverse --inline-info --no-multi --height=60% --tiebreak=index --bind='?:toggle-preview' --preview='$preview'"
+
 
 # Initialize skim
 dir=${0:A:h}
 
-for file in "$dir"/*.zsh; do
-  if [[ -r "$file" && "${file:t}" != "init.zsh" ]]; then
-    source "$file"
-  fi
+files=(
+  "$dir/completion.zsh"
+  "$dir/key-bindings.zsh"
+)
+
+for file in "${files[@]}"; do
+  [[ -f "$file" ]] && source "$file"
 done
